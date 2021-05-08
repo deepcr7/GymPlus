43d01f89b66f898378d9032e113403c5c9db750b
deepcr7 <deepeshsuranjan@gmail.com>
2021-05-09 00:11:14 +0530

    added tricep video component js part

--- script.js
+++ script.js
@@ -24,7 +24,7 @@
 
           AFRAME.registerComponent('video-vidhandler2', {
             init: function() {
-    console.log('video init entered');
+          console.log('video init entered');
                 this.toggle = false;
                 this.vid = document.querySelector("#gym2");
                 this.vid.pause();
@@ -43,7 +43,27 @@
         });
           
           //Tricep Press Video Component
-          
+
+          AFRAME.registerComponent('video-vidhandler3', {
+                init: function() {
+		          console.log('video init entered');
+                    this.toggle = false;
+                    this.vid = document.querySelector("#gym3");
+                    this.vid.pause();
+                },
+                tick: function() {
+                    if (this.el.object3D.visible == true) {
+                        if (!this.toggle) {
+                            this.toggle = true;
+                            this.vid.play();
+                        }
+                    } else {
+                        this.toggle = false;
+                        this.vid.pause();
+                    }
+                }
+            });
+
 
 
           // Gym Timer for Covid Protocols
